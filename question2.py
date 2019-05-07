d8c834d63f8713e408a1818a3b1997835ede90b3
archanagottipaty <41593885+archanagottipaty@users.noreply.github.com>
2019-05-07 06:32:09 -0700

    Update question2.py

--- question2.py
+++ question2.py
@@ -15,5 +15,5 @@ for line in f:
     print(output[0])
     output2=(output[1].split(","))
     for x in output2:
-        print(x.lstrip().rstrip())
+        print(x.lstrip().rstrip()) #lstrip() removes leading whitespace and rstrip() removes trailing newlines
 
